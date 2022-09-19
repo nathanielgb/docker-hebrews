0d2c77dd3d6e345fce7df790df2b3fc6e1a02b5e
Nathaniel Bambico <63771622+nathanielgb@users.noreply.github.com>
2022-09-19 16:55:09 +0800

    Delete hebrews/vendor directory

--- hebrews/vendor/laraveldaily/larastarters/resources/stubs/breeze/inertia/notusjs/js/Components/Divider.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <hr class="border-b-1 border-blueGray-300">
-</template>
--- hebrews/vendor/laraveldaily/larastarters/resources/stubs/breeze/notusjs/views/components/divider.blade.php
+++
@@ -1,1 +0,0 @@
-<hr {{ $attributes->merge(['type' => 'submit', 'class' => 'border-b-1 border-blueGray-300']) }}>
